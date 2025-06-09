8bb40b4800476284f887ea92affcd8974adda3cc
zawerex <zadoronidanil@gmail.com>
2025-06-09 23:15:47 +0700

    Update Kavo.lua

--- Kavo.lua
+++ Kavo.lua
@@ -216,7 +216,7 @@ function Kavo.CreateLib(kavName, themeList)
     blurFrame.Name = "blurFrame"
     blurFrame.Parent = pages
     blurFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-    blurFrame.BackgroundTransparency = 1
+    blurFrame.BackgroundTransparency = 0.3
     blurFrame.BorderSizePixel = 0
     blurFrame.Position = UDim2.new(-0.0222222228, 0, -0.0371747203, 0)
     blurFrame.Size = UDim2.new(0, 376, 0, 289)
@@ -232,7 +232,7 @@ function Kavo.CreateLib(kavName, themeList)
     Main.BackgroundColor3 = themeList.Background
     Main.ClipsDescendants = true
     Main.Position = UDim2.new(0.336503863, 0, 0.275485456, 0)
-    Main.Size = UDim2.new(0, 525, 0, 318)
+    Main.Size = UDim2.new(0,600, 0, 400)
 
     MainCorner.CornerRadius = UDim.new(0, 4)
     MainCorner.Name = "MainCorner"
@@ -265,7 +265,7 @@ function Kavo.CreateLib(kavName, themeList)
     title.Font = Enum.Font.Gotham
     title.RichText = true
     title.Text = kavName
-    title.TextColor3 = Color3.fromRGB(245, 245, 245)
+    title.TextColor3 = Color3.fromRGB(255, 0, 100)
     title.TextSize = 16.000
     title.TextXAlignment = Enum.TextXAlignment.Left
 
